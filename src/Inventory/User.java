4ee356aefc491d8bc9c4ae48cffc2018be77aece
ShinyIllust <marijnhuiskes@gmail.com>
2024-11-06 13:41:23 +0100

    Assignment 37/38 progress

--- src/Inventory/User.java
+++ src/Inventory/User.java
@@ -2,7 +2,7 @@ package Inventory;
 
 public class User {
     public static void main(String[] args){
-
+        //Change
         Pack pack = new Pack(2,10,10);
         pack.add(new Arrow());
         pack.add(new Arrow());
