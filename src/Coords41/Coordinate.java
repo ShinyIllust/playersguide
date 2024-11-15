c45275907c573216d76bc83016ceab7a23b57c48
ShinyIllust <marijnhuiskes@gmail.com>
2024-11-15 15:14:25 +0100

    Assignment 41 Fix

--- src/Coords41/Coordinate.java
+++ src/Coords41/Coordinate.java
@@ -3,13 +3,15 @@ package Coords41;
 public record Coordinate(int row, int column) {
     boolean isAdjacent(int rowOther, int columnOther){
         if(row + 1 == rowOther || row - 1 == rowOther){
-            return column + 1 != columnOther && column - 1 != columnOther;
-        } else {
-            if (column + 1 == columnOther || column - 1 == columnOther){
-                return row + 1 != rowOther && row - 1 != rowOther;
-            } else {
-                return false;
+            if(column == columnOther || column == columnOther + 1 || column == columnOther - 1){
+                return true;
             }
         }
+        if(column + 1 == columnOther || column - 1 == columnOther){
+            if(row == rowOther || row == rowOther + 1 || row == rowOther - 1){
+                return true;
+            }
+        }
+        return false;
     }
 }
--- src/Coords41/Main.java
+++ src/Coords41/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args){
         Coordinate room1 = new Coordinate(0,0);
         Coordinate room2 = new Coordinate(1, 0);
-        Coordinate room3 = new Coordinate(1, 1);
-        System.out.println(room1.isAdjacent(room1.row(), room1.column()));
+        Coordinate room3 = new Coordinate(2, 0);
+        System.out.println(room1.isAdjacent(room3.row(), room3.column()));
     }
 }
